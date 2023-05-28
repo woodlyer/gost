0f56827452c02d8dbac6c45125433368fc5ad159
woodlyer <118674311+woodlyer@users.noreply.github.com>
2023-05-28 09:47:40 +0800

    Update mybuild.sh

--- mybuild.sh
+++ mybuild.sh
@@ -9,4 +9,17 @@ release(){
 }
 
 
+win()
+{ 
+  GOOS=windows GOARCH=amd64 go build  -ldflags "-s -w" ./cmd/gost
+}
+
+swager()
+{
+ go install https://github.com/go-swagger/go-swagger/cmd
+ swagger generate spec -o swagger.yaml
+ cp swagger.yaml go-gost/x/api/
+}
+
+
 debug
